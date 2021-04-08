3a50fe13519b934f62ec163b717032a45adee855
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-08 00:28:19 -0300

    feat: constant for icons :sparkles:

---
+++ src/constants/categories.ts
@@ -0,0 +1,17 @@
+export const Categories = {
+  'vida noturna': {
+    path: '/images/categories/drink.svg',
+  },
+  praia: {
+    path: '/images/categories/surf.svg',
+  },
+  moderno: {
+    path: '/images/categories/building.svg',
+  },
+  clássico: {
+    path: '/images/categories/museum.svg',
+  },
+  'e mais...': {
+    path: '/images/categories/earth.svg',
+  },
+};
