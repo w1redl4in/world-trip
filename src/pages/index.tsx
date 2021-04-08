72afd9ddcb57a7f2847775ae774d9faf91563230
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-08 17:07:45 -0300

    refactor: using header in all pages :recycle:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { Banner } from '../components/Banner';
 import { Carousel } from '../components/Carousel';
 import { Categories } from '../components/Categories';
-import { Header } from '../components/Header';
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Banner />
       <Categories />
       <Carousel />
