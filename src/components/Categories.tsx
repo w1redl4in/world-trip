d498245d36f069e580f553abc3000257b064ef62
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-08 16:36:28 -0300

    feat: some responsive treatment :sparkles:

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,28 +1,49 @@
-import { Flex, Stack, Image, Text, Divider, Center } from '@chakra-ui/react';
+import {
+  Flex,
+  Stack,
+  Image,
+  Text,
+  Divider,
+  Center,
+  useMediaQuery,
+} from '@chakra-ui/react';
 import { Categories as CategoriesIcons } from '../constants/categories';
 
 export const Categories = () => {
+  const [isLargerThan1280] = useMediaQuery('(min-width: 1280px)');
+
   return (
     <>
       <Flex
         as="section"
-        px="180"
-        h="140"
         align="center"
-        justify="space-between"
+        justify="space-around"
         mt="20"
+        flexWrap="wrap"
       >
         {Object.entries(CategoriesIcons).map(([key, value]) => (
           <Stack key={key} textAlign="center">
-            <Image src={value.path} />
-            <Text fontWeight={600} color="gray.500">
-              {key}
-            </Text>
+            <Flex direction="column" align="center" justify="space-between">
+              {isLargerThan1280 ? (
+                <>
+                  <Image boxSize="50px" src={value.path} />
+                  <Text fontWeight={600} color="gray.500">
+                    {key}
+                  </Text>
+                </>
+              ) : (
+                <>
+                  <Text fontWeight={600} color="gray.500">
+                    {key}
+                  </Text>
+                </>
+              )}
+            </Flex>
           </Stack>
         ))}
       </Flex>
       <Center height="50px">
-        <Divider orientation="horizontal" width="20" colorScheme="gray" />
+        <Divider orientation="horizontal" width="20" />
       </Center>
     </>
   );
