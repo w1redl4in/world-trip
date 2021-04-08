346991bdb88a00aed6d633af6455574fc3b7b471
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-08 18:40:59 -0300

    feat: showing arrow back to / :sparkles:

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,33 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Flex, Image, Center as Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export const Header = () => {
+  const history = useRouter();
+
   return (
     <Flex
       as="header"
       height="100"
       display="flex"
       align="center"
-      justify="center"
+      justify="space-around"
+      position="relative"
     >
-      <Image src="/images/logo.svg" alt="world trip logo" />
+      {history.pathname !== '/' && (
+        <Box
+          position="absolute"
+          left="20"
+          _hover={{
+            cursor: 'pointer',
+          }}
+        >
+          <Link href="/">
+            <img src="/images/chevron-left.svg" />
+          </Link>
+        </Box>
+      )}
+      <Image src="/images/logo.svg" alt="world trip logo" mx="auto" />
     </Flex>
   );
 };
