5581c8a3a3304251ce5bea8d7334c6f86d8d57c2
Felipe <felipe.araujo@hubfintech.com.br>
2021-04-08 23:44:39 -0300

    feat: constants carousel :sparkles:

---
+++ src/constants/carousel.ts
@@ -0,0 +1,18 @@
+export const CAROUSEL = {
+  'Europe <br /> O continente mais antigo.': {
+    path: '/images/carousel/europe.jpg',
+  },
+  'Ásia <br /> O continente com a maior população mundial': {
+    path: '/images/carousel/asia.jpg',
+  },
+  'África <br /> Terra dos animais': {
+    path: '/images/carousel/africa.jpg',
+  },
+  'América <br /> As capitais mais altas do mundo': {
+    path: '/images/carousel/america.jpg',
+  },
+
+  'Oceania <br /> É formada por apenas 19 territórios': {
+    path: '/images/carousel/oceania.jpg',
+  },
+};
